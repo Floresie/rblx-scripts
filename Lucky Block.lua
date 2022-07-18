1c8cbb9dcc75af26599ff3030f599ef95c76b43e
Floresie <105591117+Floresie@users.noreply.github.com>
2022-07-18 16:49:49 +0700

    Update Lucky Block.lua

--- Lucky Block.lua
+++ Lucky Block.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("Please wait until the script is updated / discord.gg/y32Xtxtmwj")
+game:GetService("Players").LocalPlayer:Kick("Please wait until the script is updated.")
