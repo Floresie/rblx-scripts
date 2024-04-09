1425a74ae47d61acb16caee4bbf630ba72943872
Floresie <105591117+Floresie@users.noreply.github.com>
2024-04-10 06:17:03 +0700

    Update Backpacking.lua

--- Backpacking.lua
+++ Backpacking.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("Flores's Script has been discontinued and became Lunaria Script")
+game:GetService("Players").LocalPlayer:Kick("Flores's Script has been discontinued and became Lunaria Script DM : yzhserf")
